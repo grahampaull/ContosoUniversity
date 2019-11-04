049cada159618661e6bee0277af7392bea992e73
grahampaull <grahampaull2007@gmail.com>
2019-11-04 14:54:05 +0000

    Step 2 Complete

--- Pages/Students/Edit.cshtml.cs
+++ Pages/Students/Edit.cshtml.cs
@@ -23,51 +23,42 @@ namespace ContosoUniversity.Pages.Students
         [BindProperty]
         public Student Student { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+       public async Task<IActionResult> OnGetAsync(int? id)
+{
+    if (id == null)
+    {
+        return NotFound();
+    }
 
-            Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+    Student = await _context.Students.FindAsync(id);
 
-            if (Student == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
+    if (Student == null)
+    {
+        return NotFound();
+    }
+    return Page();
+}
 
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+public async Task<IActionResult> OnPostAsync(int id)
+{
+    var studentToUpdate = await _context.Students.FindAsync(id);
 
-            _context.Attach(Student).State = EntityState.Modified;
+    if (studentToUpdate == null)
+    {
+        return NotFound();
+    }
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(Student.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+    if (await TryUpdateModelAsync<Student>(
+        studentToUpdate,
+        "student",
+        s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+    {
+        await _context.SaveChangesAsync();
+        return RedirectToPage("./Index");
+    }
 
-            return RedirectToPage("./Index");
-        }
+    return Page();
+}
 
         private bool StudentExists(int id)
         {
