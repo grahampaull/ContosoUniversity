6e9ccafac53aea408370fbe86c0f01a173f5e407
grahampaull <grahampaull2007@gmail.com>
2019-11-06 15:51:54 +0000

    Finished Part 5

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,4 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ContosoUniversity.Models
+
 {
     public enum Grade
     {
@@ -10,6 +14,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
